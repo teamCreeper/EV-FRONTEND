353abb22f4048442eed9c618c623a784fafac66a
joo1124 <tmddjsdla@naver.com>
2024-09-12 22:26:20 +0900

    :sparkles:3_create_Audi/Bmw/Benzmodel_components
    I cretated Audi, Bmw, Benz components. Also, added the images for the components.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import MainCar from './MainCar';
-import { Searchbar } from './Searchbar';
-import Hyundaimodel from './Hyundaimodel';
-import Swipertest from './Swipertest';
-import Kiamodel from './Kiamodel';
+import MainCar from "./MainCar";
+import { Searchbar } from "./Searchbar";
+import Hyundaimodel from "./Hyundaimodel";
+import Swipertest from "./Swipertest";
+import Kiamodel from "./Kiamodel";
+import Audimodel from "./Audimodel";
+import Bmwmodel from "./Bmwmodel";
+import Benzmodel from "./Benzmodel";
 
 // 스타일을 객체로 분리
 
@@ -15,6 +18,9 @@ function Main() {
       <Searchbar />
       <Hyundaimodel />
       <Kiamodel />
+      <Audimodel />
+      <Bmwmodel />
+      <Benzmodel />
       <Swipertest />
     </div>
   );
